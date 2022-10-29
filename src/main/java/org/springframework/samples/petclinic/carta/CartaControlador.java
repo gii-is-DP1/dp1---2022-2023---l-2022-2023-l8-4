560c6373bd10e2984f945a0208d367a89f8cd941
carlos-bermejo <carbersor@alum.us.es>
2022-10-29 13:06:09 +0200

    controlador carta

--- src/main/java/org/springframework/samples/petclinic/carta/CartaControlador.java
+++ src/main/java/org/springframework/samples/petclinic/carta/CartaControlador.java
@@ -1,8 +1,11 @@
 package org.springframework.samples.petclinic.carta;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,4 +22,12 @@ public class CartaControlador {
 		result.addObject("cartas", cartaServicio.findAll());
 		return result;
 	}
+	
+	@GetMapping("/cartas/{cartaId}")
+	public ModelAndView mostrarCarta(@PathVariable("cartaId") int cartaId) {
+		ModelAndView mav = new ModelAndView("cartas/cartaDetalles");
+		mav.addObject(this.cartaServicio.findOwnerById(cartaId));
+		return mav;
+	}
+	
 }
