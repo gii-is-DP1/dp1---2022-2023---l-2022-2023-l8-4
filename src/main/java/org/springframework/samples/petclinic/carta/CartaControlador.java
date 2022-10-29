aaea402a5f26ce1fba80ad6a923c3c38e4c45fd0
carbersor <carbersor@alum.us.es>
2022-10-29 12:37:08 +0200

    cartacontrolador

--- src/main/java/org/springframework/samples/petclinic/carta/CartaControlador.java
+++ src/main/java/org/springframework/samples/petclinic/carta/CartaControlador.java
@@ -2,12 +2,21 @@ package org.springframework.samples.petclinic.carta;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 @RequestMapping("/cartas")
 public class CartaControlador {
 	@Autowired
-	private CartaServicio boardsService;
+	private CartaServicio cartaServicio;
+	
+	@GetMapping
+	public ModelAndView listadoCartas(){		
+		ModelAndView result=new ModelAndView("ListaDeCartas");		
+		result.addObject("cartas", cartaServicio.findAll());
+		return result;
+	}
 }
