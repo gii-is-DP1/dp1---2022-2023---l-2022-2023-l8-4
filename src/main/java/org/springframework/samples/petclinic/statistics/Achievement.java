6b4c31a461ccfbe1e188df6450cbd14236f8439d
pedlopruz <plopezr2011@gmail.com>
2022-11-11 16:37:10 +0100

    Actualizacion entidad logros

--- src/main/java/org/springframework/samples/petclinic/statistics/Achievement.java
+++ src/main/java/org/springframework/samples/petclinic/statistics/Achievement.java
@@ -1,6 +1,10 @@
 package org.springframework.samples.petclinic.statistics;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.samples.petclinic.model.NamedEntity;
 
@@ -10,9 +14,15 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(name = "achievement")
 public class Achievement extends NamedEntity {
+	@Column(name = "description")
+	@NotEmpty
     private String description;
+	@Column(name = "badgeImage")
     private String badgeImage;
+	@Column(name = "threshold")
+	@NotNull
     private double threshold;
     
     public String getActualDescription(){
--- src/main/java/org/springframework/samples/petclinic/statistics/AchievementService.java
+++ src/main/java/org/springframework/samples/petclinic/statistics/AchievementService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AchievementService {
@@ -14,19 +15,19 @@ public class AchievementService {
     public AchievementService(AchievementRepository repo){
         this.repo=repo;
     }
-
+    @Transactional(readOnly = true)
     List<Achievement> getAchievements(){
         return repo.findAll();
     }
-
+    @Transactional(readOnly = true)
     public Achievement getById(int id){
         return repo.findById(id).get();
     }
-
+    @Transactional
     public void deleteAchievementById(int id){
         repo.deleteById(id);
     }
-
+    @Transactional
     public void save(Achievement achievement){
         repo.save(achievement);
     }
