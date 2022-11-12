ee2f0871d267e82b748349f1979e6be28704e8e0
Pedro González Marcos <pedgonmar2@alum.us.es>
2022-11-12 15:38:06 +0100

    Removed unused import

--- src/main/java/org/springframework/samples/petclinic/card/CardRepository.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardRepository.java
@@ -2,7 +2,6 @@ package org.springframework.samples.petclinic.card;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
