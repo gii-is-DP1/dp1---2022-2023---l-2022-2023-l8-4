2aa0e8665f3b1ce17dccee3c5f6f9e46d57a24f6
Pedro González Marcos <pedgonmar2@alum.us.es>
2022-11-14 09:45:44 +0100

    Added game mode formatter and parametrized tests

---
+++ src/test/java/org/springframework/samples/petclinic/partida/GameModeFormatterTest.java
@@ -0,0 +1,67 @@
+package org.springframework.samples.petclinic.partida;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.text.ParseException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.samples.petclinic.game.GameMode;
+import org.springframework.samples.petclinic.game.GameModeFormatter;
+
+public class GameModeFormatterTest {
+
+	private GameModeFormatter gameModeFormatter;
+	
+	@BeforeEach
+	void setup() {
+		this.gameModeFormatter = new GameModeFormatter();
+	}
+	
+	@Test
+	void shouldPrintGameMode() {
+		String estandarName = "ESTANDAR";
+		String elFosoName = "EL FOSO";
+		String patataCalienteName = "LA PATATA CALIENTE";
+		GameMode estandar = GameMode.ESTANDAR;
+		GameMode elFoso = GameMode.EL_FOSO;
+		GameMode patataCaliente = GameMode.LA_PATATA_CALIENTE;
+
+
+		
+		String actualEstandar = gameModeFormatter.print(estandar, null);
+		String actualFoso = gameModeFormatter.print(elFoso, null);
+		String actualPatataCaliente = gameModeFormatter.print(patataCaliente, null);
+
+		assertEquals(estandarName, actualEstandar, "Estandar name does not match");
+		assertEquals(elFosoName, actualFoso, "El FOSO mode does not match");
+		assertEquals(patataCalienteName, actualPatataCaliente, "PATATA CALIENTE does not match");
+
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/GameModeFormatterPositiveValues.csv", delimiter=';')
+	void shouldParseGameMode(String gameMode, String value) {
+		
+		try {
+			GameMode actualEstandar = gameModeFormatter.parse(value, null);
+			assertEquals(GameMode.valueOf(gameMode), actualEstandar, "Estandar name does not match");
+			
+		} catch (ParseException e) {
+			fail();
+		}
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/GameModeFormatterNegativeValues.csv", delimiter=';')
+	void shouldThrowParseException(String gameMode, String value) {
+		
+			assertThrows(ParseException.class, () -> {gameModeFormatter.parse(value, null);});
+			
+	}
+	
+}
