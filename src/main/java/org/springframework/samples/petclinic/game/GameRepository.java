678297698d4c5ff3f70bb2f5ab2f8cb47d1e5a93
Moffinguer <fermago11@gmail.com>
2022-11-19 10:44:28 +0100

    [Bugfix] Added deleted function for user profile, statistics Signed-off-by: Moffinguer <fermago11@gmail.com>
    	modified:   src/main/java/org/springframework/samples/petclinic/game/GameRepository.java
    	modified:   src/main/java/org/springframework/samples/petclinic/game/GameService.java
    	modified:   src/main/java/org/springframework/samples/petclinic/player/PlayerController.java

--- src/main/java/org/springframework/samples/petclinic/game/GameRepository.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameRepository.java
@@ -1,24 +1,28 @@
 package org.springframework.samples.petclinic.game;
 
 import java.util.Collection;
-import java.util.List; 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, Integer> {
-	
+
 	List<Game> findAll();
 	List<Game> findGamesByOrderByDateDesc();
 	Optional<Game> findById(int id);
 	List<Game> findGamesByGameStateOrderByDateDesc(GameState gameState);
-	
+
 	@Query(value="SELECT games.id, date, game_code, game_mode, game_state FROM games "
 			+ "JOIN players_games ON games.id=players_games.game_id "
 			+ "JOIN players  ON players_games.player_id=players.id "
 			+ "WHERE players.id=?1", nativeQuery= true)
 	Collection<Game> findGamesByPlayerId(int id);
+
+    @Query( "SELECT g FROM Game g JOIN g.players p WHERE p.id = :id" )
+    List<Game> getGamesByPlayers( @Param("id") int playerId );
 }
--- src/main/java/org/springframework/samples/petclinic/game/GameService.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameService.java
@@ -13,61 +13,66 @@ import org.springframework.transaction.annotation.Transactional;
 public class GameService {
 
 	private final GameRepository gameRepository;
-		
+
 	@Autowired
 	public GameService(GameRepository gameRepository) {
 		this.gameRepository = gameRepository;
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<Game> getGames() throws DataAccessException {
 		return this.gameRepository.findAll();
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<Game> getGamesInProgress() throws DataAccessException {
 		return this.gameRepository.findGamesByGameStateOrderByDateDesc(GameState.IN_PROGRESS);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<Game> getGamesFinalized() throws DataAccessException {
 		return this.gameRepository.findGamesByGameStateOrderByDateDesc(GameState.FINALIZED);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Collection<Game> getGamesByPlayerId(int id) throws DataAccessException {
 		return this.gameRepository.findGamesByPlayerId(id);
 	}
-	
-	
+
+
 	@Transactional(readOnly = true)
 	public List<Game> getGamesByDateDesc() throws DataAccessException {
 		return this.gameRepository.findGamesByOrderByDateDesc();
-	}	
-	
+	}
+
 	@Transactional(readOnly = true)
 	public Game getGameById(int id) throws DataAccessException {
 		return this.gameRepository.findById(id).orElse(null);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Collection<Player> getPlayersFromGame(int gameId) throws Exception {
 		Game game = getGameById(gameId);
-		
+
 		if (game == null) {
 			throw new Exception("Game not found");
 		}
-		
+
 		return game.getPlayers();
 	}
-	
+
 	@Transactional
 	public void saveGame(Game game) throws DataAccessException {
 		this.gameRepository.save(game);
 	}
-	
+
 	@Transactional
 	public void deleteGame(int gameid) throws DataAccessException {
 		this.gameRepository.deleteById(gameid);
-	}	
+	}
+
+    @Transactional
+    public List<Game> gamesByPlayers(Integer id) {
+        return this.gameRepository.getGamesByPlayers( id );
+    }
 }
